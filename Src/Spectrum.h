96a70a0febf09b29a15e0c54b27f46936436dfd6
404Kurama <kurama.notfound@gmail.com>
2023-04-15 15:21:33 +0700

    feat(spectrum): add static

--- Src/Spectrum.h
+++ Src/Spectrum.h
@@ -17,6 +17,32 @@ namespace Spectrum {
 
     namespace Static {
         const unsigned int NONE = 0x00000000;
+        const unsigned int WHITE = Color(0xFFFFFF);
+        const unsigned int BLACK = Color(0x000000);
+        const unsigned int GRAY200 = Color(0xF4F4F4);
+        const unsigned int GRAY300 = Color(0xEAEAEA);
+        const unsigned int GRAY400 = Color(0xD3D3D3);
+        const unsigned int GRAY500 = Color(0xBCBCBC);
+        const unsigned int GRAY600 = Color(0x959595);
+        const unsigned int GRAY700 = Color(0x767676);
+        const unsigned int GRAY800 = Color(0x505050);
+        const unsigned int GRAY900 = Color(0x323232);
+        const unsigned int BLUE400 = Color(0x378EF0);
+        const unsigned int BLUE500 = Color(0x2680EB);
+        const unsigned int BLUE600 = Color(0x1473E6);
+        const unsigned int BLUE700 = Color(0x0D66D0);
+        const unsigned int RED400 = Color(0xEC5B62);
+        const unsigned int RED500 = Color(0xE34850);
+        const unsigned int RED600 = Color(0xD7373F);
+        const unsigned int RED700 = Color(0xC9252D);
+        const unsigned int ORANGE400 = Color(0xF29423);
+        const unsigned int ORANGE500 = Color(0xE68619);
+        const unsigned int ORANGE600 = Color(0xDA7B11);
+        const unsigned int ORANGE700 = Color(0xCB6F10);
+        const unsigned int GREEN400 = Color(0x33AB84);
+        const unsigned int GREEN500 = Color(0x2D9D78);
+        const unsigned int GREEN600 = Color(0x268E6C);
+        const unsigned int GREEN700 = Color(0x12805C);
     }
 
     const unsigned int GRAY50 = Color(0x252525);
