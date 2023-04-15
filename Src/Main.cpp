59084a5a515b4da77be8fd36363ba6bd5b25de9d
404Kurama <kurama.notfound@gmail.com>
2023-04-15 16:16:20 +0700

    feat(main): loader

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,10 +1,63 @@
 #include "Gui.h"
+#include "Memory.hpp"
 
 #include <iostream>
 
 int main() {
 	std::cout << "[X] -->> Welcome to Astra <<-- [X]" << std::endl;
+	std::cout << "I don't know why i can't make this console hide with c++, I think you need to do it yourself!\n" << std::endl;
 
+	// Getting Game Process
+	DWORD ProcessId = Memory::GetProcessId(L"csgo.exe");
+
+	if (!ProcessId) {
+		std::cout << "Waiting for CS:GO..." << std::endl;
+
+		while (!ProcessId) {
+			ProcessId = Memory::GetProcessId(L"csgo.exe");
+			Sleep(200UL);
+		}
+	}
+
+	DWORD BaseAddress = Memory::GetModuleAddress(ProcessId, L"csgo.exe");
+	DWORD ClientAddress = Memory::GetModuleAddress(ProcessId, L"client.dll");
+	DWORD EngineAddress = Memory::GetModuleAddress(ProcessId, L"engine.dll");
+
+	if (EngineAddress == 0 || BaseAddress == 0 || EngineAddress == 0) {
+		std::cout << "Waiting for CS:GO to load..." << std::endl;
+
+		while (ClientAddress == 0 || BaseAddress == 0 || EngineAddress == 0) {
+			BaseAddress = Memory::GetModuleAddress(ProcessId, L"csgo.exe");
+			ClientAddress = Memory::GetModuleAddress(ProcessId, L"client.dll");
+			EngineAddress = Memory::GetModuleAddress(ProcessId, L"engine.dll");
+			Sleep(200UL);
+		}
+	}
+
+	HANDLE Handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId);
+
+	if (!Handle) {
+		std::cout << "Couldn't get CS:GO handle." << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	Globals::ProcessHandle = Handle;
+	Globals::ClientAddress = ClientAddress;
+	Globals::EngineAddress = EngineAddress;
+
+	// Start
+	std::cout << "----------------------------------------" << std::endl;
+	std::cout << "Base Process Id: " << ProcessId << std::endl;
+	std::cout << "Base Address: " << BaseAddress << std::endl;
+	std::cout << "Client Address: " << ClientAddress << std::endl;
+	std::cout << "----------------------------------------" << std::endl;
+
+	std::cout << "\nFound game process starting astra in 3s" << std::endl;
+	Sleep(3000UL);
+	ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
+
+	// Render Gui
 	Gui::RenderGui();
 
 	return 0;
