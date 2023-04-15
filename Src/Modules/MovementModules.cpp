bed2501384868a263b641269d551aaaa8efd98a8
404Kurama <kurama.notfound@gmail.com>
2023-04-15 16:52:39 +0700

    feat(modules/movement): idk what i did

--- Src/Modules/MovementModules.cpp
+++ Src/Modules/MovementModules.cpp
@@ -1,18 +1,28 @@
 #include "MovementModules.h"
 #include "../Memory.hpp"
 #include "../Gui.h"
+#include "../Offsets.hpp"
 
 #include <iostream>
 #include <thread>
 
 void MovementModules::StartMovementModules() noexcept {
 	while (Gui::IsRunning) {
-		StartBhopModule();
+		LocalPlayer = Memory::Read<uintptr_t>(Globals::ClientAddress + Offsets::signatures::dwLocalPlayer);
+
+		if (LocalPlayer) {
+			LocalPlayerHealth = Memory::Read<std::int32_t>(LocalPlayer + Offsets::netvars::m_iHealth);
+			LocalPlayerLifestate = Memory::Read<std::int32_t>(LocalPlayer + Offsets::netvars::m_lifeState);
+			LocalPlayerTeam = Memory::Read<std::int32_t>(LocalPlayer + Offsets::netvars::m_iTeamNum);
+			LocalPlayerFlags = Memory::Read<std::int32_t>(LocalPlayer + Offsets::netvars::m_fFlags);
+
+			if (Globals::BHOPEnabled) StartBhopModule();
+		}
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 }
 
 void MovementModules::StartBhopModule() noexcept {
-	
+	std::cout << LocalPlayerHealth << "\n";
 }
--- Src/Modules/MovementModules.h
+++ Src/Modules/MovementModules.h
@@ -1,6 +1,13 @@
 #pragma once
+#include <iostream>
 
 namespace MovementModules {
+	inline uintptr_t LocalPlayer;
+	inline std::int32_t LocalPlayerHealth;
+	inline std::int32_t LocalPlayerLifestate;
+	inline std::int32_t LocalPlayerTeam;
+	inline std::int32_t LocalPlayerFlags;
+
 	void StartMovementModules() noexcept;
 
 	void StartBhopModule() noexcept;
